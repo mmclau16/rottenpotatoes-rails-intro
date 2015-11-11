03baacb42f14af72417445098d561f5a859dd2b6
mmclau16 <mmclau16@uncc.edu>
2015-11-10 23:59:55 -0500

    hw 2 part 3 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -5,9 +5,8 @@ class MoviesController < ApplicationController
   end
 
   def show
-    id = params[:id] # retrieve movie ID from URI route
-    @movie = Movie.find(id) # look up movie by unique ID
-    # will render app/views/movies/show.<extension> by default
+    id = params[:id] 
+    @movie = Movie.find(id) 
   end
 
   def index
